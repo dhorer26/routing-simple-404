8b50770730a1810b7bdfe83f7d906fd3cd5117df
Ravindra Dhore <ravindrad@ansh.com>
2019-11-26 17:52:44 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { StudntDetailComponent } from './studnt-detail/studnt-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  {path: 'ViewStudents', component: StudentListComponent},
+  {path: 'ViewEmployees', component: EmployeeListComponent},
+  {path: 'studentDetail', component: StudntDetailComponent},
+  {path: '', redirectTo: '/ViewStudents', pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent}  // 404 page not found should be at last
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentListComponent,
+    EmployeeListComponent,
+    StudntDetailComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/studnt-detail/studnt-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studnt-detail',
+  templateUrl: './studnt-detail.component.html'
+})
+export class StudntDetailComponent {
+}
